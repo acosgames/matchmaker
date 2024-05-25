52b681ff6b2538a031d5bcb9678d609ba01d1374
Joel <joelruiz2@gmail.com>
2024-05-25 01:06:14 -0500

    fixing stats and season

--- src/playerreceiver.js
+++ src/playerreceiver.js
@@ -1,35 +1,43 @@
-const rabbitmq = require('shared/services/rabbitmq');
-const redis = require('shared/services/redis');
-const profiler = require('shared/util/profiler');
-const events = require('./events');
-const queuemanager = require('./queuemanager');
+const rabbitmq = require("shared/services/rabbitmq");
+const redis = require("shared/services/redis");
+const profiler = require("shared/util/profiler");
+const events = require("./events");
+const queuemanager = require("./queuemanager");
 
 class PlayerReceiver {
-
-    constructor() {
-
-    }
+    constructor() {}
 
     sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+        return new Promise((resolve) => setTimeout(resolve, ms));
     }
 
     start() {
         return new Promise(async (rs, rj) => {
-            while (!rabbitmq.isActive() || !redis.isActive) {
+            while (!rabbitmq.isActive() || !redis.isActive()) {
                 console.warn("[MatchMaker] waiting on rabbitmq and redis...");
                 await this.sleep(1000);
             }
 
-
             await queuemanager.loadQueues();
 
-            await rabbitmq.subscribeQueue('leaveParty', this.onLeaveParty.bind(this));
-            await rabbitmq.subscribeQueue('joinParty', this.onJoinParty.bind(this));
-            await rabbitmq.subscribeQueue('joinQueue', this.onJoinQueue.bind(this));
-            await rabbitmq.subscribeQueue('leaveQueue', this.onLeaveQueue.bind(this));
+            await rabbitmq.subscribeQueue(
+                "leaveParty",
+                this.onLeaveParty.bind(this)
+            );
+            await rabbitmq.subscribeQueue(
+                "joinParty",
+                this.onJoinParty.bind(this)
+            );
+            await rabbitmq.subscribeQueue(
+                "joinQueue",
+                this.onJoinQueue.bind(this)
+            );
+            await rabbitmq.subscribeQueue(
+                "leaveQueue",
+                this.onLeaveQueue.bind(this)
+            );
             rs(true);
-        })
+        });
     }
 
     async onLeaveParty(msg) {
